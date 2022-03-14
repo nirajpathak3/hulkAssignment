9de6dd82a80773ea0d7dae36f80d05c7bec56b1e
Neeraj Pathak <nirajanand.pathak@gmail.com>
2022-03-14 12:28:37 +0530

    update migration file

---
+++ database/migrations/2022_03_14_054735_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('post_title');
+            $table->text('post_content');
+            $table->string('post_url');
+            $table->string('post_type');
+            $table->enum('post_status', ['publish','unpublish'])->default('publish');  
+            $table->integer('total_submit_count');
+            $table->integer('total_open_count');          
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
